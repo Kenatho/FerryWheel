bda98126b55893057acb844df977dd21678f1b01
Kenath Liyanage <jallerken@hotmail.se>
2023-03-23 12:31:55 +0100

    experimenting with wheel cart axis

--- lua/main.lua
+++ lua/main.lua
@@ -12,19 +12,50 @@ local app = App(client)
 -- Assets are files (images, glb models, videos, sounds, etc...) that you want to use
 -- in your app. They need to be published so that user's headsets can download them
 -- before you can use them. We make `assets` global so you can use it throughout your app.
+
+--These are imported models that become assets
+
 assets = {
     quit = ui.Asset.File("images/quit.png"),
-    ferris_wheel = ui.Asset.File("models/ThemePark.glb"),
+    ferris_wheelstand = ui.Asset.File("models/FerryWheelStand.glb"),
+    ferris_wheel = ui.Asset.File("models/FerryWheel.glb"),
+    ferris_wheelcart = ui.Asset.File("models/FerryWheelCart.glb")
 }
 app.assetManager:add(assets)
 
-local bounds = ui.Bounds(0, 0, 0, 1, 1, 1)
-local asset = assets.ferris_wheel
+-- These are the assets that are visible in Allo (axis: x, y, z, scale: 1, 1, 1)
 
-local ferris_view = ui.ModelView(bounds, asset)
-app.mainView = ferris_view
+local bounds_for_wheelstand = ui.Bounds(0, 0, 0, 1, 1, 1)
+local wheelstand_view = ui.ModelView
+(bounds_for_wheelstand, assets.ferris_wheelstand)
 
-    
+local bounds_for_wheel = ui.Bounds(0, 0, 0, 1, 1, 1)
+local wheel_view = ui.ModelView
+(bounds_for_wheel, assets.ferris_wheel)
+
+local bounds_for_wheelcart = ui.Bounds(0, 0, 0, 1, 1, 1)
+local wheelcart_view = ui.ModelView
+(bounds_for_wheelstand, assets.ferris_wheelcart)
+
+-- This is the the Parent/Child objects
+
+wheelstand_view:addSubview(wheel_view)
+wheel_view:addSubview(wheelcart_view)  
+
+
+app.mainView = wheelstand_view
+
+-- This is the animation to the assets
+
+app:scheduleAction(0.05, true, function ()
+    -- function bounds:rotate(angle, x, y, z) d
+        wheel_view:setBounds(wheel_view.bounds:rotate(0.03, 0, 0, 1))
+
+            -- function bounds:rotate(angle, x, y, z)
+          --       wheelcart_view:setBounds(wheelcart_view.bounds:rotate(0.03, 0, 0, -1))
+end)
+
+
 
 
 -- Connect to the designated remote Place server
@@ -32,3 +63,4 @@ app:connect()
 -- hand over runtime to the app! App will now run forever,
 -- or until the app is shut down (ctrl-C or exit button pressed).
 app:run()
+
